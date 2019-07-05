f1828b58dbeab32dc5d17d146bc18e02718c20a4
jerrinvarghese <30927067+jerrinvarghese@users.noreply.github.com>
2019-07-05 17:54:29 +0530

    Add project files.

---
+++ DAL/Repository/ItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class ItemRepository
+    {
+        public List<tbl_Login> getLoginTableData()
+        {
+            DbForLearningEntities context = new DbForLearningEntities();
+            List<tbl_Login> loginList = context.tbl_Login.ToList();
+            return (loginList);
+        }
+    }
+}
---
+++ MVC_Application1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Repository;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+
+namespace MVC_Application1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ItemRepository loginData = new ItemRepository();
+            List<tbl_Login> lst = loginData.getLoginTableData();
+            return View();
+        }
+    }
+}
