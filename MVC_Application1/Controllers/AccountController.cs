e17381db75bc3c14a800680e3aa6a9eb76741e51
jerrinvarghese <30927067+jerrinvarghese@users.noreply.github.com>
2019-07-23 11:39:10 +0530

    New login page added. Form downloaded from internet. Using jquery, the login and register page are showing on single page. On clicking the signup, the register form will be shown hiding the login form.

---
+++ MVC_Application1/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
